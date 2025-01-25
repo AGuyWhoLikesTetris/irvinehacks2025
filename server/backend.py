c4410f6a30c3d320c055ef2dd1d28a9c2fb43719
Jaden Suh <jadenjs1@uci.edu>
2025-01-24 23:53:13 -0800

    Adding Classes Works
    /add_classes endpoint can be used to add classes to a user.

--- server/backend.py
+++ server/backend.py
@@ -33,31 +33,60 @@ def index():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("SELECT * FROM student")
-    tasks = c.fetchall()
+    users = c.fetchall()
     conn.close()
-    return tasks
+    return users
 
 @app.route('/add', methods=['POST'])
-def add_task():
+def add_user():
+    '''Requires id, name, degree, grade in form data'''
     id = flask.request.form['id']
     name = flask.request.form['name']
+    degree = flask.request.form['degree']
+    grade = flask.request.form['grade']
     if name:
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
-        c.execute("INSERT INTO student (id, name) \
-                    VALUES (?, ?)", (id, name))
+        c.execute("INSERT INTO student (id, name, degree, grade) \
+                    VALUES (?, ?, ?, ?)", (id, name, degree, grade))
         conn.commit()
         conn.close()
     return flask.redirect(flask.url_for('index'))
 
 @app.route('/delete/<int:id>')
-def delete_task(id):
+def delete_user(id):
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("DELETE FROM student WHERE id=?", (id,))
     conn.commit()
     conn.close()
     return flask.redirect(flask.url_for('index'))
+
+@app.route('/view/<int:id>')
+def view(id):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM student WHERE id=?", (id,))
+    user = c.fetchone()
+    c.execute("SELECT course_id FROM enrollment WHERE id=?", (id,))
+    courses = c.fetchall()
+    conn.close()
+    rtn_obj = {'id': user[0], 'name': user[1], 'degree': user[2], 'grade': user[3], 'courses': courses}
+    return rtn_obj
+
+@app.route('/add_classes', methods=['POST'])
+def add_classes():
+    id = flask.request.form['id']
+    classes = flask.request.form['classes']
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    print(classes)
+    for course_id in classes:
+        print(course_id)
+        c.execute("INSERT INTO enrollment (id, course_id) \
+                    VALUES (?, ?)", (id, course_id))
+    conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
     init_db()
